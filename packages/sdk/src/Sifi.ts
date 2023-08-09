44fdd0d5c8d8b24a8bbcd5ce0afc77d51829d2c4
Xykota <xykota@proton.me>
2023-08-09 17:34:36 +0700

    feat(sdk): add sdk

---
+++ packages/sdk/src/Sifi.ts
@@ -0,0 +1,112 @@
+type GetQuoteOptions = {
+  fromToken: string;
+  toToken: string;
+  fromAmount: bigint | string;
+};
+
+type Quote = {
+  id: string;
+  toAmount: bigint;
+  estimatedGas: bigint;
+  approveAddress: string;
+  toAmountAfterFeesUsd: string;
+};
+
+type GetSwapOptions = {
+  /**
+   * Quote ID or Quote object.
+   */
+  quote: Quote | string;
+  /**
+   * Address of the token to swap from.
+   */
+  fromAddress: string;
+  /**
+   * Slippage as fraction of 1 (e.g. 0.005 for 0.5%)
+   */
+  slippage?: number;
+  /**
+   * Recipient of swapped tokens. Defaults to `fromAddress`.
+   */
+  toAddress?: string;
+};
+
+export type Swap = {
+  tx: {
+    from: string;
+    to: string;
+    value: string;
+    data: string;
+    chainId: number;
+    gasLimit: string;
+  };
+  estimatedGasTotalUsd: string;
+};
+
+async function handleResponse(response: Response) {
+  const contentType = response.headers.get('content-type');
+
+  if (response.ok) {
+    if (!contentType?.startsWith('application/json')) {
+      throw new Error(
+        `Failed to get quote. Unexpected response content type: ${contentType ?? '<none>'}`
+      );
+    }
+
+    return await response.json();
+  }
+
+  if (contentType?.startsWith('application/json')) {
+    const json = (await response.json()) as { code: number; message: string };
+
+    throw new Error(`Failed to get quote: ${json.code}: ${json.message}`);
+  }
+
+  throw new Error(`Failed to get quote: ${response.statusText}`);
+}
+
+export class Sifi {
+  constructor(private readonly baseUrl = 'https://api.sideshift.fi/v1/') {}
+
+  async getQuote(options: GetQuoteOptions): Promise<Quote> {
+    const query = new URLSearchParams({
+      fromToken: options.fromToken,
+      toToken: options.toToken,
+      fromAmount: options.fromAmount.toString(),
+    }).toString();
+
+    const response = (await fetch(`${this.baseUrl}quote?${query}`).then(handleResponse)) as any;
+
+    return {
+      id: response.id,
+      toAmount: BigInt(response.toAmount),
+      estimatedGas: BigInt(response.estimatedGas),
+      approveAddress: response.approveAddress,
+      toAmountAfterFeesUsd: response.toAmountAfterFeesUsd,
+    };
+  }
+
+  async getSwap(options: GetSwapOptions): Promise<Swap> {
+    const params: Record<string, string> = {
+      quoteId: typeof options.quote === 'string' ? options.quote : options.quote.id,
+      fromAddress: options.fromAddress,
+    };
+
+    if (options.toAddress !== undefined) {
+      params.toAddress = options.toAddress;
+    }
+
+    if (options.slippage !== undefined) {
+      params.slippage = options.slippage.toString();
+    }
+
+    const query = new URLSearchParams(params).toString();
+
+    const response = (await fetch(`${this.baseUrl}swap?${query}`).then(handleResponse)) as any;
+
+    return {
+      tx: response.tx,
+      estimatedGasTotalUsd: response.estimatedGasTotalUsd,
+    };
+  }
+}
