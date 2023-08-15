902361dc00b0a150d8a1b819d59fc778271fe002
jflint256 <102540515+jflint256@users.noreply.github.com>
2023-08-15 14:29:15 +0200

    refactor(hardhat): use foundry for unit tests

---
+++ packages/hardhat/test/foundry/helpers/TestHelperContract.t.sol
@@ -0,0 +1,52 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.21;
+
+import 'forge-std/Test.sol';
+import 'contracts/Spender.sol';
+import 'contracts/SifiV1Router01.sol';
+import '@openzeppelin/contracts/token/ERC20/ERC20.sol';
+import '@uniswap/v2-periphery/contracts/interfaces/IUniswapV2Router02.sol';
+
+abstract contract TestHelperContract is Test {
+  Spender spender;
+  SifiV1Router01 sifiRouter;
+
+  ERC20 constant USDC = ERC20(0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48);
+  ERC20 constant DAI = ERC20(0x6B175474E89094C44Da98b954EedeAC495271d0F);
+  ERC20 constant WETH = ERC20(0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2);
+  IUniswapV2Router02 constant IUNIV2ROUTER =
+    IUniswapV2Router02(0x7a250d5630B4cF539739dF2C5dAcb4c659F2488D);
+
+  uint256 constant AMOUNT = 10000;
+  address constant USDCWHALEADDRESS = 0x0162Cd2BA40E23378Bf0FD41f919E1be075f025F;
+  address constant FEESADDR = 0x0C4BEf84b07dc0D84ebC414b24cF7Acce24261BA;
+  address constant EEEADDR = 0xEeeeeEeeeEeEeeEeEeEeeEEEeeeeEeeeeeeeEEeE;
+
+  uint256 deadline;
+
+  string MAINNET_RPC_URL = vm.envString('MAINNET_RPC_URL');
+
+  constructor() {
+    vm.createSelectFork(MAINNET_RPC_URL, 17853419);
+
+    spender = new Spender();
+
+    sifiRouter = new SifiV1Router01(
+      address(spender),
+      payable(FEESADDR),
+      address(WETH),
+      address(IUNIV2ROUTER)
+    );
+
+    spender.grantRole(keccak256('TRANSFER_ROLE'), address(sifiRouter));
+
+    deadline = block.timestamp + 1000;
+  }
+
+  receive() external payable {}
+
+  function test_Deployed() public {
+    assertTrue(address(spender) != address(0));
+    assertTrue(address(sifiRouter) != address(0));
+  }
+}
