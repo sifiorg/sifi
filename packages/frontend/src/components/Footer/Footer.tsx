a2c4faf51abde721c7b7922749f0c6b625471a24
Nathan Minkhorst <agent.mcallister@proton.me>
2023-08-23 15:49:32 +0200

    feat(frontend): update shared-ui to latest version

--- packages/frontend/src/components/Footer/Footer.tsx
+++ packages/frontend/src/components/Footer/Footer.tsx
@@ -1,11 +1,11 @@
 import { FunctionComponent } from 'react';
 import { Footer as FooterComponent } from '@sifi/shared-ui';
-import logo from 'src/assets/logo.svg';
+import { ReactComponent as Logo } from '../../assets/logo.svg';
 
 const Footer: FunctionComponent = () => {
   return (
     <FooterComponent
-      logo={logo}
+      Logo={Logo}
       description="Find the most efficient routes for your trades with our advanced DEX aggregator."
       columnLinks={[]}
       name="SideShift.fi"
