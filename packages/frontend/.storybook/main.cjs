ca16415bf574498574072e7cb52ed0c3a8bf6e84
Nathan Minkhorst <agent.mcallister@proton.me>
2023-08-23 19:02:29 +0200

    fix(frontend): storybook not opening

--- packages/frontend/.storybook/main.cjs
+++ packages/frontend/.storybook/main.cjs
@@ -1,18 +1,22 @@
 module.exports = {
-  stories: ["../src/**/*.stories.mdx", "../src/**/*.stories.@(js|jsx|ts|tsx)"],
-  addons: ["@storybook/addon-links", "@storybook/addon-essentials", "@storybook/addon-interactions"],
+  stories: ['../src/**/*.stories.mdx', '../src/**/*.stories.@(js|jsx|ts|tsx)'],
+  addons: [
+    '@storybook/addon-links',
+    '@storybook/addon-essentials',
+    '@storybook/addon-interactions',
+  ],
   framework: {
-    name: "@storybook/react-vite",
-    options: {}
+    name: '@storybook/react-vite',
+    options: {},
   },
   core: {},
   features: {
-    storyStoreV7: true
+    storyStoreV7: true,
   },
   docsPage: {
-    docs: "automatic"
+    docs: 'automatic',
   },
-  staticDirs: ["../public"],
+  staticDirs: ['../public'],
   docs: {
     autodocs: true,
   },
