54652e0e3bb9b8e190a36a07f142f41f50de2bd7
Nathan Minkhorst <agent.mcallister@proton.me>
2023-08-28 12:56:02 +0200

    fix(frontend): weird looking dropdown

--- packages/frontend/src/components/HeaderMenu/HeaderMenu.tsx
+++ packages/frontend/src/components/HeaderMenu/HeaderMenu.tsx
@@ -1,27 +1,37 @@
 import { FunctionComponent } from 'react';
 import { Menu } from '@sifi/shared-ui';
-import { useAccount, useEnsName, useNetwork } from 'wagmi';
+import { useAccount, useDisconnect, useEnsName, useNetwork } from 'wagmi';
 import { formatAddress, formatEnsName } from 'src/utils';
 
 const HeaderMenu: FunctionComponent = () => {
   const { address, isConnected } = useAccount();
   const { data: ensName } = useEnsName({ address });
   const { chain } = useNetwork();
+  const { disconnect } = useDisconnect();
 
   if (!address || !isConnected) return null;
 
   const label = ensName ? formatEnsName(ensName) : formatAddress(address);
 
+  const menuLinks = [
+    {
+      title: 'Disconnect Wallet',
+      onClick: () => disconnect(),
+    },
+  ];
+
   return (
-    <Menu label={label}>
-      <span className="mb-3 block text-sm" id="network-label">
-        Network
-      </span>
-      <div
-        className="font-display text-uppercase block text-base font-bold"
-        aria-labelledby="network-label"
-      >
-        {chain?.name}
+    <Menu label={label} links={menuLinks}>
+      <div className="px-6 py-4">
+        <span className="mb-3 block text-sm" id="network-label">
+          Network
+        </span>
+        <div
+          className="font-display text-uppercase block text-base font-bold"
+          aria-labelledby="network-label"
+        >
+          {chain?.name}
+        </div>
       </div>
     </Menu>
   );
