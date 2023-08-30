a995948f7d104dcbe9991e2c4b9e63c86be1f961
Nathan Minkhorst <agent.mcallister@proton.me>
2023-08-30 21:34:57 +0200

    feat(frontend): reduce swap buttons height

--- packages/frontend/src/components/Button/Button.stories.tsx
+++ packages/frontend/src/components/Button/Button.stories.tsx
@@ -1,16 +1,19 @@
-import { StoryFn, Meta } from "@storybook/react";
-import { Button } from "./Button";
+import { StoryFn, Meta } from '@storybook/react';
+import { Button } from './Button';
 
 export default {
-  title: "Components/Button",
+  title: 'Components/Button',
   component: Button,
 } as Meta<typeof Button>;
 
-const Template: StoryFn<typeof Button> = (args) => (
-  <Button {...args}>Shift</Button>
-);
+const Template: StoryFn<typeof Button> = args => <Button {...args}>Shift</Button>;
 
 export const Default = Template.bind({});
+
+export const Large = Template.bind({});
+Large.args = {
+  size: 'large',
+};
 
 export const Disabled = Template.bind({});
 Disabled.args = {
--- packages/frontend/src/components/Button/Button.tsx
+++ packages/frontend/src/components/Button/Button.tsx
@@ -3,16 +3,29 @@ import { Spinner } from '../Spinner/Spinner';
 
 type ButtonProps = {
   isLoading?: boolean;
+  size?: 'large' | 'normal';
 } & ButtonHTMLAttributes<HTMLButtonElement>;
 
-const Button: FunctionComponent<ButtonProps> = ({ children, isLoading, ...rest }) => (
-  <button
-    className="bg-primary-purple text-flashbang-white focus:ring-pixel-blue flex h-24 w-full max-w-md place-items-center justify-center rounded-md border border-none py-2 px-4 text-lg uppercase shadow-sm hover:bg-opacity-90 focus:outline-none focus:ring-2 focus:ring-offset-2 disabled:cursor-not-allowed disabled:bg-opacity-60 disabled:text-opacity-60 disabled:hover:bg-opacity-60"
-    disabled={isLoading}
-    {...rest}
-  >
-    {isLoading ? <Spinner /> : children}
-  </button>
-);
+const Button: FunctionComponent<ButtonProps> = ({
+  children,
+  isLoading,
+  size = 'normal',
+  ...rest
+}) => {
+  const styles = {
+    large: 'h-24',
+    normal: 'h-20',
+  };
+
+  return (
+    <button
+      className={`bg-primary-purple text-flashbang-white focus:ring-pixel-blue flex ${styles[size]} w-full max-w-md place-items-center justify-center rounded-md border border-none py-2 px-4 text-lg uppercase shadow-sm hover:bg-opacity-90 focus:outline-none focus:ring-2 focus:ring-offset-2 disabled:cursor-not-allowed disabled:bg-opacity-60 disabled:text-opacity-60 disabled:hover:bg-opacity-60`}
+      disabled={isLoading}
+      {...rest}
+    >
+      {isLoading ? <Spinner /> : children}
+    </button>
+  );
+};
 
 export { Button };
