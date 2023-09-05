7188c1d4be0a372ab2bd2505500d926fcea3871a
Xykota <xykota@proton.me>
2023-09-05 09:45:50 +0700

    feat(hardhat): improve DRY_RUN deploy

--- packages/hardhat/deploy/001_diamond.ts
+++ packages/hardhat/deploy/001_diamond.ts
@@ -1,27 +1,27 @@
 import { DeployFunction } from 'hardhat-deploy/types';
 import { HardhatRuntimeEnvironment } from 'hardhat/types';
-import { verify } from './helpers';
+import { deploy, verify } from './helpers';
 
 type SifiDiamondDeployArgs = [owner: string, diamondCutFacetAddress: string];
 
 const func: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
-  const { deployments, getNamedAccounts } = hre;
-  const { deploy } = deployments;
+  const { getNamedAccounts } = hre;
 
   const { defaultDeployer } = await getNamedAccounts();
 
-  const diamondCutFacet = await deploy('DiamondCutFacet', {
+  const diamondCutFacet = await deploy(hre, 'DiamondCutFacet', {
     from: defaultDeployer,
     args: [],
     log: true,
     autoMine: true,
+    skipIfAlreadyDeployed: process.env.DRY_RUN === '1',
   });
 
   await verify(diamondCutFacet.address, []);
 
   const diamondArgs: SifiDiamondDeployArgs = [defaultDeployer, diamondCutFacet.address];
 
-  const sifiDiamond = await deploy('SifiDiamond', {
+  const sifiDiamond = await deploy(hre, 'SifiDiamond', {
     from: defaultDeployer,
     args: diamondArgs,
     log: true,
