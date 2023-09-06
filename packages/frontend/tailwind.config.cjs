96b6421bd72d088e4d4df24f6ee21f912ca8446a
Nathan Minkhorst <agent.mcallister@proton.me>
2023-09-06 14:37:20 +0200

    fix(frontend): tailwind theme is based on system preference

--- packages/frontend/tailwind.config.cjs
+++ packages/frontend/tailwind.config.cjs
@@ -1,6 +1,7 @@
 const defaultTheme = require('@sifi/shared-ui/dist/tailwindTheme');
 
 module.exports = {
+  darkMode: ['class', '[data-theme="dark"]'],
   content: [
     './index.html',
     './src/**/*.{js,ts,jsx,tsx}',
