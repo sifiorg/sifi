81f2f06052f06380ff6b5f50417a0597068bb64f
immutablegoblin <112887817+immutablegoblin@users.noreply.github.com>
2023-09-12 13:43:03 +0200

    tooling(frontend): optimize wagmi deps in vite config

--- packages/frontend/vite.config.ts
+++ packages/frontend/vite.config.ts
@@ -12,6 +12,7 @@ export default defineConfig({
   },
   optimizeDeps: {
     exclude: ['@sifi/shared-ui'],
+    include: ['bn.js', 'js-sha3', 'hash.js', 'aes-js', 'scrypt-js', 'bech32'],
     esbuildOptions: {
       define: {
         global: 'globalThis',
