e9595bbc9ab55777acec2aac875df40da91e184b
immutablegoblin <112887817+immutablegoblin@users.noreply.github.com>
2023-09-15 15:42:04 +0200

    fix(frontend): optimize wagmi deps in storybook vite config (#213)

--- packages/frontend/.storybook/main.cjs
+++ packages/frontend/.storybook/main.cjs
@@ -1,3 +1,5 @@
+const { mergeConfig } = require('vite'); 
+
 module.exports = {
   stories: ['../src/**/*.stories.mdx', '../src/**/*.stories.@(js|jsx|ts|tsx)'],
   addons: [
@@ -20,4 +22,11 @@ module.exports = {
   docs: {
     autodocs: true,
   },
+  async viteFinal(config) {
+    return mergeConfig(config, {
+      optimizeDeps: {
+        include: ['bn.js', 'js-sha3', 'hash.js', 'aes-js', 'scrypt-js', 'bech32'],
+      },
+    });
+  }
 };
