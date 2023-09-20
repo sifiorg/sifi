a0a97499560cd66a25db00dfbdac2c37c86b035a
immutablegoblin <112887817+immutablegoblin@users.noreply.github.com>
2023-09-20 10:45:22 +0300

    feat(frontend): conditionally call permit2 in swap

--- packages/frontend/src/hooks/usePermit2.ts
+++ packages/frontend/src/hooks/usePermit2.ts
@@ -2,9 +2,12 @@
 import { AllowanceProvider, AllowanceTransfer, PERMIT2_ADDRESS, type PermitSingle, MaxAllowanceTransferAmount } from '@uniswap/permit2-sdk'
 import { usePublicClient, useWalletClient } from 'wagmi';
 import { publicClientToProvider, walletClientToSigner } from 'src/utils';
+import { GetSwapOptions } from '@sifi/sdk';
 
 const PERMIT_EXPIRATION = 1000 * 60 * 60 * 24 * 30; // 30 days in ms
 const PERMIT_SIG_EXPIRATION = 1000 * 60 * 30; // 30 minutes in ms
+
+type Permit2Params = GetSwapOptions['permit'];
 
 // Current time + ms, expressed in seconds for EVM
 const toDeadline = (expiration: number): number => Math.floor((Date.now() + expiration) / 1000);
@@ -15,11 +18,12 @@ const usePermit2 = () => {
 
   // Unfortunately the permit2-sdk is built to only work with ethers.js currently
   const ethersProvider = publicClientToProvider(publicClient);
-  const allowanceProvider = new AllowanceProvider(ethersProvider, PERMIT2_ADDRESS);
 
-  const constructPermitSingle = async (tokenAddress: string, user: string, spenderAddress: string): Promise<PermitSingle> => {
-    const { nonce } = await allowanceProvider.getAllowanceData(tokenAddress, user, spenderAddress);
-  
+  const constructPermitSingle = (
+    tokenAddress: string,
+    spenderAddress: string,
+    nonce: number
+  ): PermitSingle => {  
     return {
       details: {
         token: tokenAddress,
@@ -32,11 +36,10 @@ const usePermit2 = () => {
     };
   };
 
-  const signPermit2 = async (tokenAddress: string, user: string, spenderAddress: string): Promise<string> => {
+  const signPermit2 = async (permitSingle: PermitSingle, permit2Address: string): Promise<string> => {
     if (!walletClient) throw new Error('No wallet client found');
-  
-    const permitSingle = await constructPermitSingle(tokenAddress, user, spenderAddress);
-    const { domain, types, values } = AllowanceTransfer.getPermitData(permitSingle, PERMIT2_ADDRESS, publicClient.chain.id);
+
+    const { domain, types, values } = AllowanceTransfer.getPermitData(permitSingle, permit2Address, publicClient.chain.id);
 
     const ethersSigner = walletClientToSigner(walletClient);
     const signature = await ethersSigner._signTypedData(domain, types, values);
@@ -44,7 +47,35 @@ const usePermit2 = () => {
     return signature;
   };
 
-  return { constructPermitSingle, signPermit2 };
+  type Permit2Addresses = {
+    tokenAddress: string;
+    userAddress: string;
+    spenderAddress: string;
+    permit2Address: string;
+  };
+
+  const getPermit2Params = async ({
+    tokenAddress,
+    userAddress,
+    spenderAddress,
+    permit2Address,
+  }: Permit2Addresses): Promise<Permit2Params> => {
+  const allowanceProvider = new AllowanceProvider(ethersProvider, permit2Address);
+    const { nonce } = await allowanceProvider.getAllowanceData(tokenAddress, userAddress, spenderAddress);
+
+    const signature = await signPermit2(
+      constructPermitSingle(tokenAddress, spenderAddress, nonce),
+      permit2Address
+    );
+
+    return {
+      nonce,
+      deadline: toDeadline(PERMIT_EXPIRATION),
+      signature,
+    }
+  }
+
+  return { getPermit2Params };
 };
 
 export { usePermit2 };
