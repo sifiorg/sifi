74de62383045fe13241f5e222e0e72cdd54d33cd
Boris Kubrik <boriskubrik@proton.me>
2023-09-22 13:22:37 +0100

    fix(frontend): TX URL

--- packages/frontend/src/utils/evm.ts
+++ packages/frontend/src/utils/evm.ts
@@ -1,32 +1,9 @@
-const getEvmTxUrl = (network: string, txid: string): string | undefined => {
-  switch (network) {
-    case 'ethereum':
-      return `https://etherscan.io/tx/${txid}`;
-    case 'avax':
-      return `https://snowtrace.io/tx/${txid}`;
-    case 'bsc':
-      return `https://bscscan.com/tx/${txid}`;
-    case 'polygon':
-      return `https://polygonscan.com/tx/${txid}`;
-    case 'etc':
-      return `http://gastracker.io/tx/${txid}`;
-    case 'fantom':
-      return `https://ftmscan.com/tx/${txid}`;
-    case 'arbitrum':
-      return `https://arbiscan.io/tx/${txid}`;
-    case 'optimism':
-      return `https://optimistic.etherscan.io/tx/${txid}`;
-    case 'smartbch':
-      return `https://smartscan.cash/transaction/${txid}`;
-    case 'cronos':
-      return `https://cronoscan.com/tx/${txid}`;
-    case 'arbitrumnova':
-      return `https://nova.arbiscan.io/tx/${txid}`;
-    case 'zksyncera':
-      return `https://explorer.zksync.io/tx/${txid}`;
-    default:
-      return undefined;
-  }
+import { Chain } from 'viem';
+
+const getEvmTxUrl = (chain: Chain, txid: string): string | undefined => {
+  return chain.blockExplorers?.default
+    ? `${chain.blockExplorers?.default.url}/tx/${txid}`
+    : undefined;
 };
 
 export { getEvmTxUrl };
