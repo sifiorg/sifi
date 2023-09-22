bdd3c1469dcb78f781c6732f9d000bdfa8f85767
immutablegoblin <112887817+immutablegoblin@users.noreply.github.com>
2023-09-22 15:24:45 +0300

    feat(frontend): add sepolia to chainlist on localhost (#241)

--- packages/frontend/src/utils/chains.ts
+++ packages/frontend/src/utils/chains.ts
@@ -3,15 +3,22 @@ import arbitrumIcon from '../assets/chain-icons/arbitrum.svg';
 import polygonIcon from '../assets/chain-icons/polygon.svg';
 import optimismIcon from '../assets/chain-icons/optimism.svg';
 import defaultIcon from '../assets/chain-icons/default-chain.svg';
-import { mainnet, polygon, optimism, arbitrum, Chain } from 'viem/chains';
+import { mainnet, polygon, optimism, arbitrum, sepolia, Chain } from 'viem/chains';
+import { isTest } from './environments';
 
-const SUPPORTED_CHAINS: Chain[] = [
+const PRODUCTION_CHAINS: Chain[] = [
   // The order in which the chains are displayed in the UI
   mainnet,
   arbitrum,
   optimism,
-  polygon,
+  polygon
 ];
+
+const TEST_CHAINS: Chain[] = [
+  sepolia
+];
+
+const SUPPORTED_CHAINS = isTest ? PRODUCTION_CHAINS.concat(TEST_CHAINS) : PRODUCTION_CHAINS;
 
 function getChainIcon(chainId: number) {
   switch (chainId) {
