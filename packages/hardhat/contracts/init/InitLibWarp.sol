a3daf3e6cc19069a8265d5445fd577f93b21324e
Xykota <xykota@proton.me>
2023-09-24 11:52:11 +0700

    feat(hardhat): add stargate jump

--- packages/hardhat/contracts/init/InitLibWarp.sol
+++ packages/hardhat/contracts/init/InitLibWarp.sol
@@ -4,12 +4,14 @@ pragma solidity ^0.8.19;
 import {IWETH} from '@uniswap/v2-periphery/contracts/interfaces/IWETH.sol';
 import {LibWarp} from '../libraries/LibWarp.sol';
 import {IPermit2} from '../interfaces/external/IPermit2.sol';
+import {IStargateRouter} from '../interfaces/external/IStargateRouter.sol';
 
 contract InitLibWarp {
-  function init(address weth, address permit2) public {
+  function init(address weth, address permit2, address router) public {
     LibWarp.State storage s = LibWarp.state();
 
     s.weth = IWETH(weth);
     s.permit2 = IPermit2(permit2);
+    s.stargateRouter = IStargateRouter(router);
   }
 }
--- packages/hardhat/test/foundry/UniV3Like.t.sol
+++ packages/hardhat/test/foundry/UniV3Like.t.sol
@@ -53,7 +53,12 @@ abstract contract UniV3LikeTestBase is FacetTest, PermitSignature {
     IDiamondCut(address(diamond)).diamondCut(
       facetCuts,
       address(new InitLibWarp()),
-      abi.encodeWithSelector(InitLibWarp.init.selector, Addresses.weth(chainId), Addresses.PERMIT2)
+      abi.encodeWithSelector(
+        InitLibWarp.init.selector,
+        Addresses.weth(chainId),
+        Addresses.PERMIT2,
+        Addresses.stargateRouter(chainId)
+      )
     );
 
     facet = IUniV3Like(address(diamond));
