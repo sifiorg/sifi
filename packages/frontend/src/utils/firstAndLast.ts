3ac8a26caf0ed954e3375191db9ea65ccdb8623a
Boris Kubrik <boriskubrik@proton.me>
2023-09-28 17:06:42 +0100

    feat(frontend): add firstAndLast util

---
+++ packages/frontend/src/utils/firstAndLast.ts
@@ -0,0 +1,10 @@
+export const firstAndLast = (value: string, first = 4, last = 4, middle = '...') => {
+  if (value.length < first + last) {
+    return value;
+  }
+
+  const beginning = value.substring(0, first);
+  const end = value.substring(value.length - last);
+
+  return `${beginning}${middle}${end}`;
+};
