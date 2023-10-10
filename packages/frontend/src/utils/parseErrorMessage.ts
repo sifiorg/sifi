7964e91b29ab8165373d7c3e1ddc141370ae8d1c
immutablegoblin <112887817+immutablegoblin@users.noreply.github.com>
2023-10-10 18:16:49 +0300

    feat(frontend): show error toast if no quote available (#274)

--- packages/frontend/src/utils/parseErrorMessage.ts
+++ packages/frontend/src/utils/parseErrorMessage.ts
@@ -1,3 +1,5 @@
+import { Token } from "@sifi/sdk";
+
 const parseErrorMessage = (message: string) => {
   if (message.toLowerCase().includes('user rejected the request')) {
     return 'You rejected the transaction';
@@ -6,4 +8,20 @@ const parseErrorMessage = (message: string) => {
   return message;
 };
 
-export { parseErrorMessage };
+type SifiErrorOptions = {
+  fromToken: Token | null;
+  toToken: Token | null;
+};
+
+const parseSifiErrorMessage = (message: string, sifiErrorOptions?: SifiErrorOptions) => {
+  const { fromToken, toToken } = sifiErrorOptions || {};
+  const pairName = (fromToken && toToken) ? `${fromToken.symbol}/${toToken.symbol}` : 'pair';
+
+  if (message.toLowerCase().includes('failed to produce a quote')) {
+    return `No quote available for ${pairName}. Please try a different amount, network or token.`
+  }
+
+  return message;
+}
+
+export { parseErrorMessage, parseSifiErrorMessage };
