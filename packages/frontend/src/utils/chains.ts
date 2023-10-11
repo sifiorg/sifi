c69903a70528e68dbc380972f1ef82dfb80765cd
Boris Kubrik <boriskubrik@proton.me>
2023-10-11 12:31:33 +0100

    feat(frontend): add BNB Smart Chain

--- packages/frontend/src/utils/chains.ts
+++ packages/frontend/src/utils/chains.ts
@@ -5,6 +5,7 @@ import optimismIcon from '../assets/chain-icons/optimism.svg';
 import defaultIcon from '../assets/chain-icons/default-chain.svg';
 import avalancheIcon from '../assets/chain-icons/avax.svg';
 import baseIcon from '../assets/chain-icons/base.svg';
+import bscIcon from '../assets/chain-icons/bsc.svg';
 import {
   mainnet,
   polygon,
@@ -15,6 +16,7 @@ import {
   goerli,
   avalanche,
   base,
+  bsc,
 } from 'viem/chains';
 import { isTest } from './environments';
 
@@ -24,6 +26,7 @@ const PRODUCTION_CHAINS: Chain[] = [
   arbitrum,
   avalanche,
   base,
+  bsc,
   optimism,
   polygon,
 ];
@@ -46,17 +49,19 @@ function getChainIcon(chainId: number) {
       return avalancheIcon;
     case base.id:
       return baseIcon;
+    case bsc.id:
+      return bscIcon;
     default:
       return defaultIcon;
   }
 }
 
 const getChainById = (chainId: number): Chain => {
-  const chain = SUPPORTED_CHAINS.find((chain) => chain.id === chainId);
+  const chain = SUPPORTED_CHAINS.find(chain => chain.id === chainId);
 
   if (!chain) throw new Error(`Chain not found for chainId: ${chainId}`);
 
   return chain;
-}
+};
 
 export { SUPPORTED_CHAINS, getChainIcon, getChainById };
