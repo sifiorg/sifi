89c7d914240e49e2e69369f0a9a86834c7b9e2ae
Xykota <xykota@proton.me>
2023-10-14 15:08:45 +0700

    feat(hardhat): add ens ownershop change functions

--- packages/hardhat/contracts/interfaces/IEns.sol
+++ packages/hardhat/contracts/interfaces/IEns.sol
@@ -3,4 +3,10 @@ pragma solidity ^0.8.19;
 
 interface IEns {
   function ensSetReverseName(address reverseRegistrar, string memory name) external;
+
+  function ensUnwrap(address nameWrapper, bytes32 labelHash) external;
+
+  function ensSetApprovalForAll(address registry, address operator, bool approved) external;
+
+  function ensApprove(address registrar, address spender, uint256 tokenId) external;
 }
