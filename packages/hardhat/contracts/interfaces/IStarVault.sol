813a17a4c8c00c2ab429eb5918e523c1041ec532
Xykota <xykota@proton.me>
2023-10-19 11:10:37 +0700

    minor(hardhat): lint

--- packages/hardhat/contracts/interfaces/IStarVault.sol
+++ packages/hardhat/contracts/interfaces/IStarVault.sol
@@ -1,11 +1,6 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.19;
 
-import {SafeERC20} from '@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol';
-import {IERC20} from '@openzeppelin/contracts/token/ERC20/IERC20.sol';
-import {LibDiamond} from '../libraries/LibDiamond.sol';
-import {LibStarVault} from '../libraries/LibStarVault.sol';
-
 interface IStarVault {
   error EthTransferFailed();
 
