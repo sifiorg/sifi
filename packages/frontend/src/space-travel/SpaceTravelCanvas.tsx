8b612743633a985694466d985ec4965c3f3e8df0
Boris Kubrik <128667801+boriskubrik@users.noreply.github.com>
2023-10-20 16:38:10 +0100

    feat(frontend): add space travel (#347)
    * feat: add space-travel lib
    
    * feat: add space travel background
    
    * fix: remove redundant hook call
    
    * perf: reduce asset size
    
    * perf: further reduce asset size with

---
+++ packages/frontend/src/space-travel/SpaceTravelCanvas.tsx
@@ -0,0 +1,7 @@
+const SPACE_TRAVEL_CANVAS_ID = 'space-travel';
+
+const SpaceTravelCanvas = () => {
+  return <canvas id={SPACE_TRAVEL_CANVAS_ID} className="fixed w-screen h-screen" />;
+};
+
+export { SpaceTravelCanvas, SPACE_TRAVEL_CANVAS_ID };
