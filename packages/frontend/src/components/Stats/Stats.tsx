b40be4a17672f8ba0ba1177559f28975c9e111ec
Boris Kubrik <boriskubrik@proton.me>
2023-10-25 08:41:57 +0100

    feat(frontend): show all-time volume
    fix: pnpm
    
    fixup! feat(frontend): show all-time volume
    
    fix: make requests concurrently
    
    fixup! feat(frontend): show all-time volume
    
    fix: font size and layout shift
    
    fixup! feat(frontend): show all-time volume
    
    fix: reduce request frequency

---
+++ packages/frontend/src/components/Stats/Stats.tsx
@@ -0,0 +1,23 @@
+import { useAllTimeStats } from 'src/hooks/useAllTimeStats';
+import { numberWithCommas } from 'src/utils/numberWithCommas';
+
+const Stats = () => {
+  const { error, data } = useAllTimeStats();
+
+  if (error) {
+    console.error('Failed to fetch all-time stats:', error);
+  }
+
+  return (
+    <div className="font-display m-auto text-center text-sm p-2 flex place-items-center justify-center relative">
+      {data ? (
+        `All-time volume: $${numberWithCommas(data)}`
+      ) : (
+        // To avoid layout shifts
+        <span className="invisible">Placehodler</span>
+      )}
+    </div>
+  );
+};
+
+export { Stats };
