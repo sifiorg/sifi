896ace43d4f3c5af14bd209130f6c5daf4d61ab5
Boris Kubrik <128667801+boriskubrik@users.noreply.github.com>
2023-11-08 13:53:40 +0000

    feat(frontend): remove header chain selector (#414)

--- packages/frontend/src/components/Header/Header.tsx
+++ packages/frontend/src/components/Header/Header.tsx
@@ -2,13 +2,11 @@ import { FunctionComponent } from 'react';
 import { Navbar } from '@sifi/shared-ui';
 import logo from 'src/assets/logoWhite.svg';
 import HeaderMenu from '../HeaderMenu/HeaderMenu';
-import { HeaderChainSelector } from '../HeaderChainSelector/HeaderChainSelector';
 
 const Header: FunctionComponent = () => {
   return (
     <>
       <Navbar logo={logo}>
-        <HeaderChainSelector />
         <HeaderMenu />
       </Navbar>
     </>
