d32a2acbc789142c1b326c522d5895d860aed63a
Xykota <xykota@proton.me>
2023-11-09 16:28:26 +0700

    refactor: fix sifi branding

--- packages/frontend/src/utils/api.ts
+++ packages/frontend/src/utils/api.ts
@@ -1,3 +1,6 @@
-const baseUrl = import.meta.env.REACT_APP_BACKEND_OVERRIDE === 'localhost' ? 'http://localhost:3100/v1/' : 'https://api.sideshift.fi/v1/';
+const baseUrl =
+  import.meta.env.REACT_APP_BACKEND_OVERRIDE === 'localhost'
+    ? 'http://localhost:3100/v1/'
+    : 'https://api.sifi.org/v1/';
 
 export { baseUrl };
