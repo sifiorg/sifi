6503176bbe6fe76b677f6551cbeb1731dc98d2f4
Boris Kubrik <boriskubrik@proton.me>
2023-11-22 14:59:31 +0000

    fix: logo

--- packages/frontend/src/components/Header/Header.tsx
+++ packages/frontend/src/components/Header/Header.tsx
@@ -3,10 +3,12 @@ import { Navbar } from '@sifi/shared-ui';
 import logo from 'src/assets/logoWhite.svg';
 import HeaderMenu from '../HeaderMenu/HeaderMenu';
 
+const Logo = () => <img src={logo} alt="Sifi logo" className="max-h-[2rem]" />;
+
 const Header: FunctionComponent = () => {
   return (
     <>
-      <Navbar Logo={<img src="logo" alt="Sifi logo" />}>
+      <Navbar Logo={Logo}>
         <HeaderMenu />
       </Navbar>
     </>
