febc2d0d06d248006c717e316da6c7c4a7fd9d08
Boris Kubrik <boriskubrik@proton.me>
2023-11-23 09:06:14 +0000

    fix: use SVGR

--- packages/frontend/src/components/Header/Header.tsx
+++ packages/frontend/src/components/Header/Header.tsx
@@ -1,14 +1,12 @@
 import { FunctionComponent } from 'react';
 import { Navbar } from '@sifi/shared-ui';
-import logo from 'src/assets/logoWhite.svg';
+import { ReactComponent as Logo } from 'src/assets/logoWhite.svg';
 import HeaderMenu from '../HeaderMenu/HeaderMenu';
-
-const Logo = () => <img src={logo} alt="Sifi logo" className="max-h-[2rem]" />;
 
 const Header: FunctionComponent = () => {
   return (
     <>
-      <Navbar Logo={Logo}>
+      <Navbar Logo={<Logo alt="Sifi logo" className="max-h-[2rem]" />}>
         <HeaderMenu />
       </Navbar>
     </>
