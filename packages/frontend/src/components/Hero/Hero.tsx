eaf48fd83db58d20fcf3d1c80d179582cd736659
Boris Kubrik <boriskubrik@proton.me>
2023-12-14 13:32:34 +0000

    hotfix(frontend): site under maintantance

--- packages/frontend/src/components/Hero/Hero.tsx
+++ packages/frontend/src/components/Hero/Hero.tsx
@@ -5,18 +5,29 @@ import { Stats } from '../Stats/Stats';
 const Hero = () => (
   <div className="mx-auto flex w-full max-w-[67rem] min-h-[90vh] flex-wrap justify-center items-center px-4 py-4 sm:px-8 lg:px-16 ">
     <div className="w-full md:w-[50%] relative top-[-5vh]">
+      <div className="text-[8rem] text-center">⚠️</div>
       <h1 className="text-flashbang-white font-display mb-0 pb-0 text-center text-[1.875rem] sm:text-[2.25rem]">
-        Swap
+        Galactic Maintenance In Progress
       </h1>
       <div className="border-darker-gray mx-auto max-w-[27rem] border-b-[1px] pb-2">
-        <CreateSwap />
+        {/* <CreateSwap /> */}
+        <p className="text-center pt-4">
+          Alert: WAGMI, our interstellar wallet conduit, has encountered a cosmic anomaly. To
+          safeguard our users in the Sifi galaxy, we have temporarily suspended all wallet
+          connections.
+        </p>
+        <p className="text-center pt-4">
+          Advisory: Please initiate protocol disconnection for your wallet across all spaceports,
+          including Sifi. Refrain from engaging with any digital applications (dApps) in the cosmic
+          web until the space-time continuum is secure.
+        </p>
       </div>
-      <p className="text-smoke font-text mx-auto text-center text-base font-light md:max-w-[380px] pt-6 pb-6">
+      {/* <p className="text-smoke font-text mx-auto text-center text-base font-light md:max-w-[380px] pt-6 pb-6">
         {texts.DESCRIPTION}
       </p>
       <div className="mx-auto max-w-[27rem] ">
         <Stats />
-      </div>
+      </div> */}
     </div>
   </div>
 );
