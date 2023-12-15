e9005c4e6d0ee3644a47075f339bcb9ebfd92e3e
Boris Kubrik <boriskubrik@proton.me>
2023-12-15 19:43:39 +0000

    style: preven layout shift

--- packages/frontend/src/components/ResarchPoints/ResearchPoints.tsx
+++ packages/frontend/src/components/ResarchPoints/ResearchPoints.tsx
@@ -4,7 +4,13 @@ import { motion } from 'framer-motion';
 const ResearchPoints = () => {
   const { data } = useResearchPoints();
 
-  if (data === null || data === undefined) return null;
+  if (data === null || data === undefined)
+    return (
+      // Placeholder to prevent layout shift
+      <div className="text-right relative bottom-3 font-display text-base pr-5 sm:bottom-0 invisible">
+        <span className="text-2xl relative top-[0.075rem] pr-1"></span>
+      </div>
+    );
 
   return (
     <motion.div initial={{ opacity: 0 }} animate={{ opacity: 1 }} transition={{ duration: 0.5 }}>
