0096e09246146ccf5dd263ef47e4dc854bdb7dbf
Boris Kubrik <boriskubrik@proton.me>
2023-12-15 20:13:34 +0000

    fix: use an SVG instead of an emoji

--- packages/frontend/src/components/ResarchPoints/ResearchPoints.tsx
+++ packages/frontend/src/components/ResarchPoints/ResearchPoints.tsx
@@ -1,4 +1,5 @@
 import { useResearchPoints } from 'src/hooks/useResearchPoints';
+import { ReactComponent as AtomIcon } from 'src/assets/atom.svg';
 import { motion } from 'framer-motion';
 
 const ResearchPoints = () => {
@@ -7,15 +8,16 @@ const ResearchPoints = () => {
   if (data === null || data === undefined)
     return (
       // Placeholder to prevent layout shift
-      <div className="text-right relative bottom-3 font-display text-base pr-5 sm:bottom-0 invisible">
-        <span className="text-2xl relative top-[0.075rem] pr-1"></span>
+      <div className="relative bottom-2 font-display text-base pr-5 sm:bottom-0 sm:top-1 max-w-7xl m-auto flex justify-end items-center invisible">
+        <AtomIcon className="relative mr-1 h-5 w-5" />
+        <span className="invisible">Placeholder</span>
       </div>
     );
 
   return (
     <motion.div initial={{ opacity: 0 }} animate={{ opacity: 1 }} transition={{ duration: 0.5 }}>
-      <div className="text-right relative bottom-3 font-display text-base pr-5 sm:bottom-0 max-w-7xl m-auto">
-        <span className="text-2xl relative top-[0.075rem] pr-1">⚛️</span>
+      <div className="relative bottom-2 font-display text-base pr-5 sm:bottom-0 sm:top-1 max-w-7xl m-auto flex justify-end items-center">
+        <AtomIcon className="relative mr-1 h-5 w-5" />
         <span>{data}</span>
       </div>
     </motion.div>
