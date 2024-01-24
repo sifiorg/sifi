9c430d079da1059c6382d3fad0c251001038eaf2
immutablegoblin <112887817+immutablegoblin@users.noreply.github.com>
2024-01-24 21:03:28 +0100

    feat(frontend): prompt user to use mevblocker once (#338)

--- packages/frontend/src/utils/localStorageKeys.ts
+++ packages/frontend/src/utils/localStorageKeys.ts
@@ -2,6 +2,7 @@ const localStorageKeys = {
   REFFERRER_ADDRESS: 'referrerAddress',
   REFERRER_FEE_BPS: 'referrerFeeBps',
   SWAP_HISTORY: 'swapHistory',
+  HAS_SUGGESTED_PROTECTED_RPC: 'hasSuggestedProtectedRpc',
 } as const;
 
 export { localStorageKeys };
