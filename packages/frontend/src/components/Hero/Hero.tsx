3139d8c0988dc4f7eefe69d72b7fe40e6c980696
Boris Kubrik <boriskubrik@proton.me>
2024-01-24 15:42:57 +0000

    style(frontend): fix medium breakpoint width

--- packages/frontend/src/components/Hero/Hero.tsx
+++ packages/frontend/src/components/Hero/Hero.tsx
@@ -5,7 +5,7 @@ import { Audits } from '../Audits/Audits';
 
 const Hero = () => (
   <div className="mx-auto flex w-full max-w-[67rem] min-h-[90vh] flex-wrap justify-center items-center px-4 py-4 sm:px-8 lg:px-16 ">
-    <div className="w-full md:w-[50%] relative">
+    <div className="w-full relative">
       <h1 className="text-flashbang-white font-display mb-0 pb-0 text-center text-[1.875rem] sm:text-[2.25rem]">
         Swap
       </h1>
