f2f8c370b1835af618021fa8a87dbff2d0527761
Boris Kubrik <boriskubrik@proton.me>
2024-02-14 12:48:46 +0000

    refactor: separate Jump status polling into a hook

--- packages/frontend/src/hooks/useSwapToast.tsx
+++ packages/frontend/src/hooks/useSwapToast.tsx
@@ -1,17 +1,76 @@
+import { useEffect, useState } from 'react';
 import { showToast, Toast, updateToast } from '@sifi/shared-ui';
-import { getEvmTxUrl, getViemErrorMessage } from 'src/utils';
+import { JumpStatus } from '@sifi/sdk';
 import { usePublicClient } from 'wagmi';
+import { getEvmTxUrl, getViemErrorMessage } from 'src/utils';
 import { useSpaceTravel } from 'src/providers/SpaceTravelProvider';
 import { useSwapFormValues } from './useSwapFormValues';
-import { useSifi } from 'src/providers/SDKProvider';
 import { useRefetchBalances } from './useRefetchBalances';
+import { useJumpStatus } from './useJumpStatus';
 
 const useSwapToast = () => {
-  const sifi = useSifi();
   const { fromChain, toChain } = useSwapFormValues();
   const { refetchAllBalances } = useRefetchBalances();
   const publicClient = usePublicClient({ chainId: fromChain.id });
   const { setThrottle } = useSpaceTravel();
+  const {
+    status: jumpStatus,
+    isFetching,
+    startPolling: startPollingJumpStatus,
+    stopPolling: stopPollingJumpStatus,
+  } = useJumpStatus();
+  const isJump = fromChain.id !== toChain.id;
+  const [swapToastId, setSwapToastId] = useState<string | number | null>(null);
+  const [hash, setHash] = useState<`0x${string}` | null>(null);
+  const [explorerLink, setExplorerLink] = useState<string | undefined>(undefined);
+
+  const updateJumpToast = (status: JumpStatus) => {
+    const toastLink = explorerLink ? { text: 'View Jump', href: explorerLink } : undefined;
+
+    let toastContent = {};
+
+    switch (status) {
+      case 'pending':
+        toastContent = {
+          render: (
+            <Toast
+              text="In hyperspace. Arrival imminent..."
+              type="loading"
+              link={toastLink}
+              hideCloseIcon
+            />
+          ),
+        };
+        break;
+      case 'inflight':
+        setThrottle(0.25);
+        toastContent = {
+          render: <Toast text="Arriving..." type="loading" link={toastLink} hideCloseIcon />,
+        };
+        break;
+      case 'success':
+        refetchAllBalances();
+        setThrottle(0.01);
+        toastContent = {
+          render: <Toast text="Jump completed." type="success" link={toastLink} />,
+          closeOnClick: true,
+        };
+        stopPollingJumpStatus();
+        break;
+      default:
+        break;
+    }
+
+    if (swapToastId && Object.keys(toastContent).length > 0) {
+      updateToast(swapToastId, toastContent);
+    }
+  };
+
+  useEffect(() => {
+    if (hash && isFetching && isJump && jumpStatus && swapToastId) {
+      updateJumpToast(jumpStatus);
+    }
+  }, [hash, isFetching, isJump, jumpStatus, swapToastId]);
 
   const showErrorToast = (error: any) => {
     if (error instanceof Error) {
@@ -22,12 +81,10 @@ const useSwapToast = () => {
   };
 
   const showSwapToast = async ({ hash }: { hash: `0x${string}` }) => {
-    const isJump = fromChain.id !== toChain.id;
-    let explorerLink: string | undefined;
     if (isJump) {
-      explorerLink = `https://layerzeroscan.com/tx/${hash}`;
+      setExplorerLink(`https://layerzeroscan.com/tx/${hash}`);
     } else {
-      explorerLink = fromChain ? getEvmTxUrl(fromChain, hash) : undefined;
+      setExplorerLink(fromChain ? getEvmTxUrl(fromChain, hash) : undefined);
     }
 
     const swapToastId = showToast({
@@ -37,55 +94,16 @@ const useSwapToast = () => {
       closeOnClick: false,
       autoClose: false,
     });
+    setSwapToastId(swapToastId);
 
     await publicClient.waitForTransactionReceipt({ hash });
+    setHash(hash);
     refetchAllBalances();
 
     // TODO: What if the transaction fails?
 
     if (isJump) {
-      const intervalId = setInterval(async () => {
-        const result = await sifi.getJump(hash);
-
-        if (result.status === 'pending') {
-          updateToast(swapToastId, {
-            render: (
-              <Toast
-                text="In hyperspace. Arrival imminent..."
-                type="loading"
-                link={explorerLink ? { text: 'View Jump', href: explorerLink } : undefined}
-                hideCloseIcon
-              />
-            ),
-          });
-        } else if (result.status === 'inflight') {
-          setThrottle(0.25);
-          updateToast(swapToastId, {
-            render: (
-              <Toast
-                text="Arriving..."
-                type="loading"
-                link={explorerLink ? { text: 'View Jump', href: explorerLink } : undefined}
-                hideCloseIcon
-              />
-            ),
-          });
-        } else if (result.status === 'success') {
-          refetchAllBalances();
-          setThrottle(0.01);
-          updateToast(swapToastId, {
-            render: (
-              <Toast
-                text="Jump completed."
-                type="success"
-                link={explorerLink ? { text: 'View Jump', href: explorerLink } : undefined}
-              />
-            ),
-            closeOnClick: true,
-          });
-          clearInterval(intervalId);
-        }
-      }, 1000);
+      startPollingJumpStatus(hash);
     }
 
     if (!isJump) {
